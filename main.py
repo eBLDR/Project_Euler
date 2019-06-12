de1483ce9f45aeb41dc271262fbdb21f193d1241
eBLDR <ed.bldr@gmail.com>
2019-06-12 11:28:03 +0200

    Ignore .pyc

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Supervisor:
     def __init__(self):
         self.interpreter = 'python'
         self.project_url = 'https://projecteuler.net/'
-        self.dir_path = './problems'
+        self.dir_path = os.path.join(os.getcwd(), 'problems')
         self.problem_number = 0
         self.base_filename = 'p{}.py'
         self.filename = ''
@@ -48,14 +48,18 @@ class Supervisor:
         if self.search_file():
             print('Computing...')
             t_0 = time()
-            ext_script = subprocess.Popen([self.interpreter, self.file_path],
-                                          stdout=subprocess.PIPE)
+            ext_script = subprocess.Popen(
+                [self.interpreter, self.file_path],
+                stdout=subprocess.PIPE
+            )
             out, err = ext_script.communicate()
             t_delta = time() - t_0
             print(
                 'Result of problem {} is: \033[1;32m{}'
-                '\033[0mCPU took {:.4} seconds'
-                .format(self.problem_number, self.bytes_to_str(out), t_delta))
+                '\033[0mCPU took {:.4} seconds'.format(
+                    self.problem_number, self.bytes_to_str(out), t_delta)
+            )
+
         else:
             print('\033[1;31mProblem not found.\033[0m')
 
